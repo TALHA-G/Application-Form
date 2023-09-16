931f31384ac4eefd24aa7d98c45aeb8c09d9e99c
Talha-G <mtalhagujjar285@gmail.com>
2023-09-16 17:31:44 +0500

    Checked

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const studentModel = require('./routes/form')
 
 app.use(express.json())
 
-app.get("/form/registration", studentModel);
+app.use("/form/registration", studentModel);
 
 app.set("view engine", "ejs")
 app.use(express.static('public'))
@@ -15,7 +15,7 @@ app.get("/form", (req, res) => {
     res.render('index')
 })
 
-mongoose.connect("mongodb://127.0.0.1:27017").then((res) => {
+mongoose.connect("mongodb://127.0.0.1:27017/Talha").then((res) => {
     console.log("Connected...")
 }).catch((error) => {
     console.log(error.message)
--- routes/form.js
+++ routes/form.js
@@ -3,9 +3,15 @@ const formModel = require("../models/formModel")
 
 
 
-  router.get("/form/registration", async function (req, res) {
-    var form = await formModel.create(req.query)
+  router.get("/", async function (req, res) {
+    await formModel.create(req.query)
     res.send(" Your Form is Submitted Successfully!")
 })
+
+router.get("/record", async function (req, res) {
+  var form = await formModel.find()
+  res.json(form)
+})
+
 
 module.exports = router ;
